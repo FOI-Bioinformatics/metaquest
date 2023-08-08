1cb27f0a649ab0675dbe1296956a790d4cd8e31e
Andreas Sjödin <andreas.sjodin@gmail.com>
2023-08-08 19:48:38 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
     packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: GPL3 License",
         "Operating System :: OS Independent",
     ],
     install_requires=[
