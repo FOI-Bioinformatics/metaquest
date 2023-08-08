817b81a1c5e73ff79229a41eb43391b962655a7d
Andreas Sjödin <andreas.sjodin@gmail.com>
2023-08-08 19:19:56 +0300

    Reorganize code to alloq packing

--- setup.py
+++ setup.py
@@ -1,29 +1,37 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
 setup(
     name="metaquest",
-    version="0.1.0",
+    version="0.1",
+    author="Andreas Sjödin",
+    author_email="andreas.sjodin@gmail.com",
+    description="A package to analyze SRA datasets and identify containment of specified genomes.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FOI-Bioinformatics/metaquest",
     packages=find_packages(),
-    entry_points={
-        "console_scripts": [
-            "metaquest = metaquest:main",
-        ]
-    },
-    install_requires=[
-        "argparse",
-        "os",
-        "csv",
-        "logging",
-        "subprocess",
-        "pathlib",
-        "collections",
-        "pandas",
-        "Bio",
-        "urllib",
-        "lxml",
-        "upsetplot",
-        "matplotlib",
-        "numpy",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
+    install_requires=[
+        'pandas',
+        'matplotlib',
+        'seaborn',
+        'numpy',
+        'umap-learn',
+        'scikit-learn',
+        # other dependencies
+    ],
+    python_requires='>=3.6',
+    entry_points={
+        'console_scripts': [
+            'metaquest=metaquest.metaquest:main',
+        ],
+    },
 )
 
